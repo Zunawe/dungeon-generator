531a6ed0cba462c110931832f72a948532220a50
Zunawe <gyroscope15@gmail.com>
2017-02-01 19:51:37 -0700

    Added program takes dimensions as arguments and README

--- src/Testing.java
+++ src/Testing.java
@@ -1,7 +1,14 @@
 public class Testing{
     public static void main(String[] args){
+        int width = 100, height = 100;
+        try{
+            width = args.length < 1 ? width : Integer.parseInt(args[0]);
+            height = args.length < 2 ? height : Integer.parseInt(args[1]);
+        }catch(NumberFormatException e){
+
+        }
         Dungeon dungeon = new Dungeon();
-        Floor floor = new Floor(200, 100);
+        Floor floor = new Floor(width, height);
         floor.generateMap();
 
         System.out.println(floor);
